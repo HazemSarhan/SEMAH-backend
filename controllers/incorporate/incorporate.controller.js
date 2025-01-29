71764c1a5b45d52d7f9d2e7d9c2ccdf40c134ced
Hazem Megahed <hazemsarhan@Hazems-MacBook-Air.local>
2025-01-29 14:44:19 +0300

    redirect url

--- controllers/incorporate/incorporate.controller.js
+++ controllers/incorporate/incorporate.controller.js
@@ -175,6 +175,9 @@ export const handleIncorporateService = async (req, res) => {
     },
   });
 
+  const redirectUrl = `${process.env.FRONTEND_URL}/myOrders/${order.id}`;
+  // res.redirect(redirectUrl);
+
   res.status(StatusCodes.OK).json({
     order,
     payment,
