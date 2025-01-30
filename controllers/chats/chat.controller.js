1ba76821eeed578d52e4014b157bd298ee2d9039
Hazem Megahed <hazemsarhan@Hazems-MacBook-Air.local>
2025-01-30 13:03:45 +0300

    redirect after incorporate

--- controllers/chats/chat.controller.js
+++ controllers/chats/chat.controller.js
@@ -22,6 +22,7 @@ export const getChats = async (req, res) => {
         employee: { select: { name: true, email: true } },
         appointment: true,
         client: { select: { commissioner: true } },
+        incorporationService: true,
       },
     });
 
